1d4bd3ee9d6165c60f768c4cb850fde1cddadcb9
Ravikishan <ravikishan1996@gmail.com>
2024-11-22 19:36:33 +0530

    2

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,9 +1,5 @@
 variable "project" {
-<<<<<<< HEAD
   description = "streaming-app"
-=======
-  description = "travelmem"
->>>>>>> ae074c3cdfb0b166640ed121796a6b02c391efe8
   type = string
 }
 
@@ -11,18 +7,11 @@ variable "project" {
 variable "aws_region" {
   description = "this will define the aws region"
   type = string
-<<<<<<< HEAD
   default = "ap-south-1"
-=======
-  default = "ap-northeast-2"
+
 }
 
-variable "aws_ami" {
-  description = "this will tell aws ami for ubuntu"
-  type = string
-  default = "ami-05d2438ca66594916"
->>>>>>> ae074c3cdfb0b166640ed121796a6b02c391efe8
-}
+
 
 # Public Subnet CIDRs
 variable "public_subnet_cidrs" {
@@ -41,7 +30,7 @@ variable "private_subnet_cidrs" {
 variable "az" {
   description = "List of az"
   type = list(string)
-<<<<<<< HEAD
+
   default = [ "ap-south-1a", "ap-south-1b" ]
 }
 
@@ -49,12 +38,3 @@ variable "az" {
 #   description = "EC2 Instance Type"
 #   default     = "t3.micro"
 # }
-=======
-  default = [ "ap-northeast-2a", "ap-northeast-2b" ]
-}
-
-variable "instance_type" {
-  description = "EC2 Instance Type"
-  default     = "t3.micro"
-}
->>>>>>> ae074c3cdfb0b166640ed121796a6b02c391efe8
